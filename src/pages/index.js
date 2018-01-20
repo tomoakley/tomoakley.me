e586dab67107f6e9fa172916df4e3ef84929760b
imin-tom <tom@imin.co>
2018-01-20 11:13:42 +0000

    Update readme to reflect new project status and replace react-boilerplate with Gatsby, and add the Articles/Projects section headers to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,6 +26,39 @@ const IntroText = styled.p`
   white-space: pre-line;
 `;
 
+const SubsectionContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 30px;
+`;
+
+const Subsection = styled.div`
+  flex: 1 1 50%;
+`;
+
+const SectionHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  background: ${(props) => props.theme.primary};
+  color: #EBF3F6;
+  font-family: ${props => props.theme.fonts.sansSerifBold};
+  font-size: 22px;
+  font-size: 2.2rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  padding: 10px 15px;
+`;
+
+const SectionHeaderTagline = styled.span`
+  font-family: ${(props) => props.theme.fonts.sansSerif};
+  font-size: 16px;
+  font-size: 1.6rem;
+  font-weight: normal;
+  text-transform: none;
+  font-style: normal;
+`;
+
 export default class IndexPage extends React.Component {
 
   static propTypes = {
@@ -45,6 +78,20 @@ export default class IndexPage extends React.Component {
             <ProfilePic src={content.image.file.url} alt={content.image.title} />
             <IntroText>{content.content.content}</IntroText>
           </TopSection>
+          <SubsectionContainer>
+            <Subsection>
+              <SectionHeader>
+                Articles
+                <SectionHeaderTagline>Assortment of coding, design, and life</SectionHeaderTagline>
+              </SectionHeader>
+            </Subsection>
+            <Subsection>
+              <SectionHeader>
+                Projects
+                <SectionHeaderTagline>Stuff I've worked on which I'm proud of</SectionHeaderTagline>
+              </SectionHeader>
+            </Subsection>
+          </SubsectionContainer>
         </div>
       </div>
     )
