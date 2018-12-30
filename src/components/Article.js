c9b68d7f5808798b33996c269e189681cf79e100
imin-tom <tom@imin.co>
2018-12-30 00:10:46 +0000

    Fix build error in netlify

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components';
-import Link from 'gatsby-link'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
 import marked from 'marked'
 import { format } from 'date-fns'
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import Link from 'gatsby-link'
+import { Link } from 'gatsby'
 import marked from 'marked'
 
 import Block from './Block'
